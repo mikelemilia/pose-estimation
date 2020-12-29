6696589a348567342be453443624b6403b0d359b
Mikele Milia <mikele.milia@studenti.unipd.it>
2020-12-29 15:36:01 +0100

    initial structure

---
+++ include/results_writer.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+
+class ResultsWriter
+{  
+public:
+  
+  ResultsWriter( const std::string &dataset_name, 
+                 const std::string &results_folder =  "."  );
+  bool addResults( const std::string &test_image_name, 
+                   const std::string &match_template_name, 
+                   int pos_x, int pos_y );
+  bool write();
+
+private:
+  
+  struct ResItem
+  {
+    ResItem(const std::string &template_name, int x, int y ) :
+      template_name(template_name), x(x), y(y){};
+    std::string template_name;
+    int x, y;
+  };
+  
+  std::map< std::string, std::vector<ResItem> > results_;
+  std::string results_file_;
+};
---
+++ src/results_writer.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+
+#include "results_writer.h"
+
+
+ResultsWriter::ResultsWriter( const std::string &dataset_name, 
+                              const std::string &results_folder ) : 
+  results_file_(results_folder + "/"  + dataset_name + "_results.txt"){}
+
+bool ResultsWriter::addResults( const std::string &test_image_name, 
+                                const std::string &match_template_name, 
+                                int pos_x, int pos_y )
+{
+  auto iter = results_.find(test_image_name);
+  if( iter != results_.end() )
+  {
+    if(iter->second.size() >= 10 )
+    {
+      std::cout<<"Error: Only the first ten best matches are required, discarding the current result"<<std::endl;
+      return false;
+    }
+  }
+  results_[test_image_name].push_back(ResItem(match_template_name, pos_x, pos_y));
+  return true;
+}
+
+bool ResultsWriter::write()
+{
+  if(results_.size() != 10 )
+  {
+    std::cout<<"Can't write the results: The algorithm must be tested on"<<std::endl;
+    std::cout<<"exactly 10 images (test0.jpg, ..., test9.jpg) for each dataset"<<std::endl;
+    return false;
+  }
+  
+  for( auto &p : results_ )
+  {
+    if( p.second.size() != 10 )
+    {
+      std::cout<<"Can't write the results: For each test image"<<std::endl;
+      std::cout<<"exactly ten best matches are required"<<std::endl;
+      return false;
+    }
+  }
+
+  std::ofstream out_file;
+  out_file.open(results_file_);
+  
+  
+  for( auto &p : results_ )
+  {
+    out_file<<p.first<<" ";
+
+    for( auto &res : p.second )
+    {
+      out_file<<res.template_name<<" "<<res.x<<" "<<res.y<<" ";
+    }
+    out_file<<std::endl;
+  }
+  
+  out_file.close();
+  
+  return true;
+}
+
